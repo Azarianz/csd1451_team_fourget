ab3c22530dfa960e68df34ee35f37a9971f92f40
adleylian <adleylianshien@gmail.com>
2026-03-26 22:59:13 +0800

    Fixed file path for settings
    -Changed settings.txt to be inside main Asset folder
    -Synchronised both versions to follow txt file in main Asset folder

--- Source/GameSettings.h
+++ Source/GameSettings.h
@@ -18,10 +18,16 @@ namespace GameSettings
 
     extern std::string selectedLevelFile;
 
+    inline std::string GetSettingsPath()
+    {
+        return "../../Assets/settings.txt";
+    }
+
     inline void Save()
     {
+        std::string p = GetSettingsPath();
         FILE* f = nullptr;
-        fopen_s(&f, "Assets/settings.txt", "w");
+        fopen_s(&f, p.c_str(), "w");
         if (!f) return;
         fprintf(f, "%d\n%d\n", resolutionIndex, masterVolume);
         fclose(f);
@@ -29,8 +35,9 @@ namespace GameSettings
 
     inline void Load()
     {
+        std::string p = GetSettingsPath();
         FILE* f = nullptr;
-        fopen_s(&f, "Assets/settings.txt", "r");
+        fopen_s(&f, p.c_str(), "r");
         if (!f) return;
         fscanf_s(f, "%d\n%d\n", &resolutionIndex, &masterVolume);
         fclose(f);
